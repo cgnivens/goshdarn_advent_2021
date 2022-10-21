2b96452f92c3ac5c7fcdc489c06f9f8c850e456c
Cory Nivens <cgn7634@gmail.com>
2022-10-20 21:18:00 -0400

    Adding comments and fixing docstrings

--- src/day1.rs
+++ src/day1.rs
@@ -44,36 +44,36 @@
 ///
 ///
 /// Part 2:
-// Instead, consider sums of a three-measurement sliding window. Again considering the above example:
+/// Instead, consider sums of a three-measurement sliding window. Again considering the above example:
 //
-// 199  A
-// 200  A B
-// 208  A B C
-// 210    B C D
-// 200  E   C D
-// 207  E F   D
-// 240  E F G
-// 269    F G H
-// 260      G H
-// 263        H
-// Start by comparing the first and second three-measurement windows. The measurements in the first window are marked A (199, 200, 208); their sum is 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210); its sum is 618. The sum of measurements in the second window is larger than the sum of the first, so this first comparison increased.
+/// 199  A
+/// 200  A B
+/// 208  A B C
+/// 210    B C D
+/// 200  E   C D
+/// 207  E F   D
+/// 240  E F G
+/// 269    F G H
+/// 260      G H
+/// 263        H
+/// Start by comparing the first and second three-measurement windows. The measurements in the first window are marked A (199, 200, 208); their sum is 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210); its sum is 618. The sum of measurements in the second window is larger than the sum of the first, so this first comparison increased.
 //
-// Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum. So, compare A with B, then compare B with C, then C with D, and so on. Stop when there aren't enough measurements left to create a new three-measurement sum.
+/// Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum. So, compare A with B, then compare B with C, then C with D, and so on. Stop when there aren't enough measurements left to create a new three-measurement sum.
 //
-// In the above example, the sum of each three-measurement window is as follows:
+/// In the above example, the sum of each three-measurement window is as follows:
 //
-// A: 607 (N/A - no previous sum)
-// B: 618 (increased)
-// C: 618 (no change)
-// D: 617 (decreased)
-// E: 647 (increased)
-// F: 716 (increased)
-// G: 769 (increased)
-// H: 792 (increased)
-// In this example, there are 5 sums that are larger than the previous sum.
-//
-// Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
-//
+/// A: 607 (N/A - no previous sum)
+/// B: 618 (increased)
+/// C: 618 (no change)
+/// D: 617 (decreased)
+/// E: 647 (increased)
+/// F: 716 (increased)
+/// G: 769 (increased)
+/// H: 792 (increased)
+/// In this example, there are 5 sums that are larger than the previous sum.
+///
+/// Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
+///
 
 
 
@@ -82,6 +82,7 @@ use std::io::{Result, Read};
 
 
 fn parse_to_int(body: String) -> Vec<i32> {
+    // Parse the file once and collect into a vec
     body
         .split("\n")
         .map(|x| x.trim().parse::<i32>().unwrap())
@@ -90,7 +91,8 @@ fn parse_to_int(body: String) -> Vec<i32> {
 
 
 fn part_one(parsed: &Vec<i32>) -> i32 {
-
+    // offset iterator to compare if parsed[i] < parsed[i+1]
+    // sum all occurrences where this is true
     parsed
         .iter()
         .zip(parsed.iter().skip(1))
@@ -100,12 +102,15 @@ fn part_one(parsed: &Vec<i32>) -> i32 {
 
 
 fn part_two(parsed: &Vec<i32>) -> i32 {
+    // Need a
     let a = parsed
         .iter()
         .zip(parsed.iter().skip(1))
         .zip(parsed.iter().skip(2))
         .map(|((x, y), z)| x + y + z);
 
+    // I tried doing let b = a.skip(1);
+    // but the borrow checker wouldn't have it
     let b = parsed
         .iter()
         .skip(1)
@@ -123,6 +128,7 @@ fn part_two(parsed: &Vec<i32>) -> i32 {
 pub fn main() -> Result<(i32, i32)> {
     let mut fh = File::open("data/day1.txt")?;
     let mut body = String::new();
+
     fh.read_to_string(&mut body)?;
     body = body.trim().to_string();
 
@@ -138,6 +144,7 @@ pub fn main() -> Result<(i32, i32)> {
 #[cfg(test)]
 #[test]
 fn test_int_conversion() {
+    // Make sure my integer converter works
     let test = String::from("199\n200\n208\n210\n200\n207");
 
     let res = parse_to_int(test);
