a8d0b5b5a8b93ee72f021a56e3547c380736231f
Cory Nivens <cgn7634@gmail.com>
2022-10-20 21:13:42 -0400

    Renamed part 1 functions and made part two. Tests run, day1 solved!

--- src/day1.rs
+++ src/day1.rs
@@ -41,17 +41,55 @@
 ///
 /// How many measurements are larger than the previous measurement?
 ///
+///
+///
+/// Part 2:
+// Instead, consider sums of a three-measurement sliding window. Again considering the above example:
+//
+// 199  A
+// 200  A B
+// 208  A B C
+// 210    B C D
+// 200  E   C D
+// 207  E F   D
+// 240  E F G
+// 269    F G H
+// 260      G H
+// 263        H
+// Start by comparing the first and second three-measurement windows. The measurements in the first window are marked A (199, 200, 208); their sum is 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210); its sum is 618. The sum of measurements in the second window is larger than the sum of the first, so this first comparison increased.
+//
+// Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum. So, compare A with B, then compare B with C, then C with D, and so on. Stop when there aren't enough measurements left to create a new three-measurement sum.
+//
+// In the above example, the sum of each three-measurement window is as follows:
+//
+// A: 607 (N/A - no previous sum)
+// B: 618 (increased)
+// C: 618 (no change)
+// D: 617 (decreased)
+// E: 647 (increased)
+// F: 716 (increased)
+// G: 769 (increased)
+// H: 792 (increased)
+// In this example, there are 5 sums that are larger than the previous sum.
+//
+// Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
+//
+
+
 
 use std::fs::File;
 use std::io::{Result, Read};
 
 
-fn find_increasing(body: String) -> i32 {
-
-    let parsed: Vec<i32> = body
+fn parse_to_int(body: String) -> Vec<i32> {
+    body
         .split("\n")
         .map(|x| x.trim().parse::<i32>().unwrap())
-        .collect();
+        .collect()
+}
+
+
+fn part_one(parsed: &Vec<i32>) -> i32 {
 
     parsed
         .iter()
@@ -61,22 +99,74 @@ fn find_increasing(body: String) -> i32 {
 }
 
 
+fn part_two(parsed: &Vec<i32>) -> i32 {
+    let a = parsed
+        .iter()
+        .zip(parsed.iter().skip(1))
+        .zip(parsed.iter().skip(2))
+        .map(|((x, y), z)| x + y + z);
 
-pub fn main() -> Result<i32> {
+    let b = parsed
+        .iter()
+        .skip(1)
+        .zip(parsed.iter().skip(2))
+        .zip(parsed.iter().skip(3))
+        .map(|((x, y), z)| x + y + z);
+
+    a.zip(b)
+        .map(|(x, y)| (y > x) as i32)
+        .sum()
+}
+
+
+
+pub fn main() -> Result<(i32, i32)> {
     let mut fh = File::open("data/day1.txt")?;
     let mut body = String::new();
     fh.read_to_string(&mut body)?;
     body = body.trim().to_string();
-    let total = find_increasing(body);
-    Ok(total)
+
+    let values = parse_to_int(body);
+
+    let total_1 = part_one(&values);
+    let total_2 = part_two(&values);
+
+    Ok((total_1, total_2))
+}
+
+
+#[cfg(test)]
+#[test]
+fn test_int_conversion() {
+    let test = String::from("199\n200\n208\n210\n200\n207");
+
+    let res = parse_to_int(test);
+
+    assert_eq!(
+        res,
+        vec![199, 200, 208, 210, 200, 207]
+    );
 }
 
 #[cfg(test)]
 #[test]
-fn run_sample() {
+fn test_part_one() {
     let test = String::from("199\n200\n208\n210\n200\n207\n240\n269\n260\n263");
+    let res = parse_to_int(test);
 
-    let tot = find_increasing(test);
+    let tot = part_one(&res);
 
     assert_eq!(tot, 7);
+}
+
+
+#[cfg(test)]
+#[test]
+fn test_part_two() {
+    let test = String::from("199\n200\n208\n210\n200\n207\n240\n269\n260\n263");
+    let res = parse_to_int(test);
+
+    let tot = part_two(&res);
+
+    assert_eq!(tot, 5);
 }
