f31b60bc74beb5247166206d46612a7d677549e2
Cory Nivens <cgn7634@gmail.com>
2022-10-20 23:14:27 -0400

    Adding tests and writing code, waited too long to commit

--- src/day2.rs
+++ src/day2.rs
@@ -26,3 +26,142 @@
 /// After following these instructions, you would have a horizontal position of 15 and a depth of 10. (Multiplying these together produces 150.)
 ///
 /// Calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
+
+
+use std::fs::File;
+use std::io;
+use std::io::{BufReader, BufRead};
+use std::default::Default;
+use std::str::FromStr;
+use std::fmt;
+
+
+type DirectionResult<T> = Result<T, ValueError>;
+
+
+#[derive(Debug, Clone, PartialEq)]
+struct ValueError;
+
+
+impl fmt::Display for ValueError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Got an invalid value for Direction!")
+    }
+}
+
+
+#[derive(Debug, PartialEq)]
+enum Direction {
+    Forward,
+    Up,
+    Down
+}
+
+impl FromStr for Direction {
+    type Err = ValueError;
+
+    fn from_str(s: &str) -> DirectionResult<Self> {
+        match s {
+            "forward" => Ok(Direction::Forward),
+            "up" => Ok(Direction::Up),
+            "down" => Ok(Direction::Down),
+            _ => Err(ValueError)
+        }
+    }
+}
+
+
+#[derive(Default)]
+struct Position {
+    x: i32,
+    depth: i32
+}
+
+impl Position {
+
+    pub fn up(&mut self, val: i32) {
+        self.depth -= val;
+    }
+
+    pub fn down(&mut self, val: i32) {
+        self.depth += val;
+    }
+
+    pub fn forward(&mut self, val: i32) {
+        self.x += val;
+    }
+}
+
+pub fn main() -> io::Result<()> {
+    let mut sub: Position = Default::default();
+
+    let fh = File::open("data/day2.txt")?;
+
+    let reader = BufReader::new(fh);
+
+    for line in reader.lines().map(|l| l.unwrap()).filter(|l| !l.trim().is_empty()) {
+
+        let mut chunk = line.split_whitespace();
+
+        let (dir, amt) = (chunk.next(), chunk.next());
+
+        let amt = amt
+            .unwrap()
+            .trim()
+            .parse::<i32>()
+            .unwrap();
+
+        let d = Direction::from_str(
+            dir.unwrap()
+        );
+
+
+        match d {
+            Ok(Direction::Up) => sub.up(amt),
+            Ok(Direction::Down) => sub.down(amt),
+            Ok(Direction::Forward) => sub.forward(amt),
+            _ => panic!("Got a weird result!")
+        };
+    }
+
+    println!("Coords for sub are:");
+    println!("x: {:?}, depth: {:?}", sub.x, sub.depth);
+    println!("Product is: {:?}", sub.x * sub.depth);
+    Ok(())
+}
+
+
+#[cfg(test)]
+#[test]
+fn test_sub_default() {
+    let sub: Position = Default::default();
+    assert_eq!((sub.x, sub.depth), (0, 0));
+}
+
+
+#[cfg(test)]
+#[test]
+fn test_sub_directions() {
+    let mut sub: Position = Default::default();
+
+    sub.forward(1);
+    sub.up(1);
+    sub.down(1);
+
+    assert_eq!((sub.x, sub.depth), (1, 0));
+}
+
+
+
+#[cfg(test)]
+#[test]
+fn test_direction_parsing() {
+    for (value, expected) in vec![
+        ("up", Direction::Up),
+        ("down", Direction::Down),
+        ("forward", Direction::Forward)
+    ].iter() {
+        let direction = Direction::from_str(value).unwrap();
+        assert_eq!(&direction, expected);
+    }
+}
