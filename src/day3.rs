8447de0c511e372ea09ba956ff9924cb3b12bcbc
Cory Nivens <cgn7634@gmail.com>
2022-10-22 17:09:54 -0400

    Updating documentations

--- src/day3.rs
+++ src/day3.rs
@@ -1,10 +1,20 @@
-// The submarine has been making some odd creaking noises, so you ask it to produce a diagnostic report just in case.
+// The submarine has been making some odd creaking noises, so you ask it
+// to produce a diagnostic report just in case.
 //
-// The diagnostic report (your puzzle input) consists of a list of binary numbers which, when decoded properly, can tell you many useful things about the conditions of the submarine. The first parameter to check is the power consumption.
+// The diagnostic report (your puzzle input) consists of a list of binary
+// numbers which, when decoded properly, can tell you many useful things
+// about the conditions of the submarine.
 //
-// You need to use the binary numbers in the diagnostic report to generate two new binary numbers (called the gamma rate and the epsilon rate). The power consumption can then be found by multiplying the gamma rate by the epsilon rate.
+// The first parameter to check is the power consumption.
 //
-// Each bit in the gamma rate can be determined by finding the most common bit in the corresponding position of all numbers in the diagnostic report. For example, given the following diagnostic report:
+// You need to use the binary numbers in the diagnostic report to generate
+// two new binary numbers (called the gamma rate and the epsilon rate).
+// The power consumption can then be found by multiplying the gamma rate
+// by the epsilon rate.
+//
+// Each bit in the gamma rate can be determined by finding the most common
+// bit in the corresponding position of all numbers in the diagnostic
+// report. For example, given the following diagnostic report:
 //
 // 00100
 // 11110
